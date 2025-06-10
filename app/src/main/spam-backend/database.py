667e233ca185c116b6f4094a4925b400f5f5d984
angelitrouss <150347699+angelitrouss@users.noreply.github.com>
2025-06-10 23:54:36 +0200

    Delete app/src/main/spam-backend directory

--- app/src/main/spam-backend/database.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-# String de conexión con credenciales reales
-client = MongoClient("mongodb+srv://root:root@cluster0.sf8nbbt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-# Selección de base de datos y colección
-db = client["detector_llamadas"]
-collection = db["usuarios"]
