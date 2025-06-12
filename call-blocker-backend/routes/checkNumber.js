69da285e73e62a2f77002480272b8365a452338d
angelitrouss <150347699+angelitrouss@users.noreply.github.com>
2025-06-12 19:52:33 +0200

    Delete call-blocker-backend directory

--- call-blocker-backend/routes/checkNumber.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { checkNumber } = require('../controllers/numberController');
-
-router.post('/check', checkNumber);
-
-module.exports = router;
